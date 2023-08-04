779f114c01e694e90d116858818e88568b14d9cf
iamwenyi <141196436+iamwenyi@users.noreply.github.com>
2023-08-04 14:32:56 +0800

    Add files via upload

--- line-plotter.py
+++ line-plotter.py
@@ -2,19 +2,29 @@ import matplotlib.pyplot as plt
 
 x_axis = [-3,-2,-1,0,1,2,3]
 y_axis = []
-
-def plotter(x_axis,y_axis):
-    plt.plot(x_axis,y_axis)
+  
+def plotter(x_axis,y_axis,gradient,y_inter):
+    line_name = "gradient = " + str(gradient) + "," + " y-intercept = " + str(y_inter)
+    plt.plot(x_axis,y_axis, label = line_name)
     plt.title('yes')
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.show()
+    plt.legend()
     
 def equation(x_axis,gradient,y_inter):
     for x in x_axis:
         y = gradient * x + y_inter
         y_axis.append(y)
-    
+
+def another_line(y_axis):
+    confirm_another = input("Would you like to plot another line?: ").lower()
+    print("")
+    if confirm_another == "yes":
+        y_axis.clear()
+        main()
+    else:
+        plt.show()
+        
 def main():
     print("Welcome to the Line Plotter!")
     print("")
@@ -22,6 +32,7 @@ def main():
     y_inter = int(input("Enter y-intercept: "))
     
     equation(x_axis,gradient,y_inter)
-    plotter(x_axis,y_axis)
+    plotter(x_axis,y_axis,gradient,y_inter)
+    another_line(y_axis)
     
 main()
