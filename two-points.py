5e822ef1730fdbe059ae1f9a83700457cf3015ae
iamwenyi <141196436+iamwenyi@users.noreply.github.com>
2023-08-09 16:29:40 +0800

    Add files via upload

---
+++ two-points.py
@@ -0,0 +1,13 @@
+def main():
+    print("Welcome! Enter two points, and I'll make the straight line equation.")
+    print("")
+    print("Point A")
+    a_xpoint = int(input("Enter x-coordinate: "))
+    a_ypoint = int(input("Enter y-coordinate: "))
+    
+    print("")
+    print("Point B")
+    b_xpoint = int(input("Enter x-coordinate: "))
+    b_ypoint = int(input("Enter y-coordinate: "))
+    
+main()
